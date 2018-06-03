7d41118299b344a1dff2dcaca761b8a99523e861
pyjune <pyjune@naver.com>
2018-06-03 21:35:31 +0900

    Create 2_6.py

---
+++ 2_6.py
@@ -0,0 +1,19 @@
+menu1 = 'Americano'
+price1 = 1500
+menu2 = 'Latte'
+price2 = 2000
+menu3 = 'Espresso'
+price3 = 1700
+menu4 = 'Mocha'
+price4 = 2500
+menu5 = '식혜'
+price5 = 2000
+menu6 = '수정과'
+price6 = 1900
+
+print(menu1, price1)
+print(menu2, price2)
+print(menu3, price3)
+print(menu4, price4)
+print(menu5, price5)
+print(menu6, price6)
