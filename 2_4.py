86972e75b79779ce2631883faac8283504423d07
pyjune <pyjune@naver.com>
2018-06-03 21:31:26 +0900

    Create 2_4.py

---
+++ 2_4.py
@@ -0,0 +1,31 @@
+# 사칙연산
+a = 3
+b = 5
+print(a+b)
+print(a+10)
+print(a-b)
+print(a*b)
+print(b*6)
+print(a/b)
+print(a/10)
+
+# 모드 연산
+print(a%2) #a를 2로 나눈 나머지
+print(b%a) #b를 a로 나눈 나머지
+
+# 거듭 제곱
+print(a**2)
+print(a**3)
+print(b**a)
+
+# 비교연산
+a = 3
+b = 5
+print(a>0)
+print(a>b)
+print(a>=3)
+print(b<10)
+print(b<=5)
+print(a==3) #a에 들은 값과 3이 같으면 True
+print(a==b) #a와 b에 들어있는 값이 같으면 True
+print(a!=b) #a와 b에 들어있는 값이 다르면 True
