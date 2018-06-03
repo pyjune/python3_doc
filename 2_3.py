e9c03041591166888d0819cb5efb88782fd751ed
pyjune <pyjune@naver.com>
2018-06-03 11:57:17 +0900

    Create 2_3.py

---
+++ 2_3.py
@@ -0,0 +1,21 @@
+# 변수와 print() 함수
+a = 2
+b = 3.14
+c = a+b
+print(a)
+print(b)
+print(c)
+print(round(c, 2))
+print("%.2f" % (c))
+print(a+b)
+print(a,b,a+b,c)
+
+# 문자열과 함께 출력
+a = 10
+print("a에 저장된 값은 ", a, "입니다.")
+
+# 지수형 표현
+d = 1e2
+e = 1e-2
+print(d)
+print(e)
