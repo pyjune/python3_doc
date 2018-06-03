85490daf4616810e3cd27644ebd7763fd985a62e
pyjune <pyjune@naver.com>
2018-06-03 21:33:50 +0900

    Create 2_5.py

---
+++ 2_5.py
@@ -0,0 +1,21 @@
+# 키보드 입력
+a = input("a = ")
+print(a)
+
+# 문자열 연산
+a = input("a = ")
+print(a)
+
+b = input("b = ")
+print(b)
+
+print("a+b = ", a+b)
+
+# 정수 연산
+a = int(input("a = "))
+print(a)
+
+b = int(input("b = "))
+print(b)
+
+print("a+b = ", a+b)
