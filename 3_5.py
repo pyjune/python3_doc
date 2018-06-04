a86aad30ec38b72a90bef96ec7b1235a99961a24
pyjune <pyjune@naver.com>
2018-06-04 16:13:32 +0900

    Create 3_5.py

---
+++ 3_5.py
@@ -0,0 +1,18 @@
+# 3번 반복 - i가 0, 1, 2인 동안 반복
+for i in range(3):
+    print(i)
+
+# i를 5, 6, 7인 동안 반복
+for i in range(5, 8):
+    print(i)
+# i의 증감 폭 지정
+for i in range(0, 6, 2):
+    print(i)
+
+for i in range(5, 0, -2):
+    print(i)
+    
+# x가 리스트의 원소
+a = [10, 20, 30]
+for x in a:
+    print(x)
