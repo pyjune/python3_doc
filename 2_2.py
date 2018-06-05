f5cef62895d9838d68d242a82a14d6ff0e5c391c
pyjune <pyjune@naver.com>
2018-06-05 16:29:32 +0900

    Update 2_2.py

--- 2_2.py
+++ 2_2.py
@@ -1,3 +1,4 @@
+# 문자열과 print()
 print("화면에 직접 출력")
 print('ab\'c')
 print("doesn't")
@@ -6,3 +7,18 @@ print('doesn\'t')
 print("'string'")
 print("\"string\"")
 print('"string"')
+
+# 문자열 저장
+s1 = '화면에 직접 출력'
+s2 = 'ab\'c'
+s3 = 'does'
+print(s1)
+print(s2)
+print(s3)
+
+# 문자열을 글자 단위로 표시하기
+print(s1[0])        # 첫 글자
+print(s2[1])        # 두 번째 글자
+print(s3[1:3])      # s3[1]부터 s3[2]까지
+print(s3[-1])       # 마지막 글자
+print(s3[-2])       # 뒤에서 두 번째
