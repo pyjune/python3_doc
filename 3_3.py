aa494d19c62701a484bf5bdd548c2b118a1b0df2
pyjune <pyjune@naver.com>
2018-06-06 17:55:17 +0900

    Update 3_3.py

--- 3_3.py
+++ 3_3.py
@@ -11,3 +11,17 @@ price = [0,1500, 2000, 1700, 2500, 2000, 1900]
 n = int(input("음료를 선택하세요 : "))
 
 print(menu[n], price[n],'원')
+
+# 전체 메뉴 보이기
+menu = ['','Americano', 'Latte', 'Espresso', 'Mocha', '식혜', '수정과']
+price = [0,1500, 2000, 1700, 2500, 2000, 1900]
+
+#메뉴 보이기
+i = 1
+while i <= 6:
+    print(i, menu[i], price[i])
+    i = i+1
+
+#메뉴 선택
+n = int(input("음료를 선택하세요 : "))
+print(menu[n], price[n],'원')
